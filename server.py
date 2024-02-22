56cee66ef8aa4ea0b859677d8b3c6203a952f13b
Aaron Heller <heller@ai.sri.com>
2024-02-22 07:39:39 -0800

    updates to client and server

--- server.py
+++ server.py
@@ -1,5 +1,10 @@
 from zero import ZeroServer
 
+STATUS_SUCCESS = 0
+STATUS_FAIL = 1
+
+state_store = {"running": False}
+
 app = ZeroServer(port=5559)
 
 
@@ -13,5 +18,27 @@ async def hello_world() -> str:
     return "hello world"
 
 
+@app.register_rpc
+def initialize() -> int:
+    return STATUS_SUCCESS
+
+
+@app.register_rpc
+def status() -> bool:
+    print("server", state_store["running"])
+    return state_store["running"]
+
+
+@app.register_rpc
+def run() -> int:
+    state_store["running"] = True
+    return STATUS_SUCCESS
+
+
+@app.register_rpc
+def get_state_store() -> dict:
+    return state_store
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(1)
--- test_client.py
+++ test_client.py
@@ -13,6 +13,27 @@ def hello():
     print(resp)
 
 
+def initialize():
+    resp = zero_client.call("initialize", None)
+    print(resp)
+
+
+def run():
+    resp = zero_client.call("run", None)
+    print(resp)
+    return resp
+
+
+def get_status():
+    resp = zero_client.call("status", None)
+    print(resp)
+    return resp
+
+
 if __name__ == "__main__":
+    initialize()
+    get_status()
+    run()
+    get_status()
     echo()
     hello()
