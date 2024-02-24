208fef73ce990033f9e1ccdf113854bef0bb7870
John Doe <johndoe@example.com>
2024-02-23 16:42:25 -0800

    updated ssh_test, added requirments

--- ssh_test.py
+++ ssh_test.py
@@ -1,12 +1,13 @@
 from fabric import Connection
 import time
 
-HOST = "192.168.2.2"
+HOST = "pi" #"192.168.2.2"
 USER = "heller"
 
 
 def update(n=10):
     with Connection(HOST, USER) as conn:
         for i in range(n):
-            result = conn.run("date", hide=True)
+            result = conn.run("date; sleep 1", hide=True)
+            result = conn.run("date >> log.txt", hide=True)
             print(result.stdout, time.time())
